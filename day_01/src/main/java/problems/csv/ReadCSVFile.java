36242236b58bd2608c25547b8dbb36a8d45f2f24
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-18 11:52:56 +0530

    [Pankaj] :  Read a CSV File and Print Data code added

---
+++ day_01/src/main/java/problems/csv/ReadCSVFile.java
@@ -0,0 +1,21 @@
+package problems.csv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFile {
+    public static void main(String[] args) throws IOException {
+        try{
+         BufferedReader br = new BufferedReader(new FileReader("csvfile.csv"));
+         String line;
+         while ((line = br.readLine()) != null) {
+             String arr[] = line.split(",");
+             System.out.println(arr[0]+" | "+arr[1]+" | "+arr[2]+" | "+arr[3]);
+         }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
